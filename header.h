52c9f79a6b1ec8e6f30e7e654a37da63ccfc9b83
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-06-20 02:34:58 +0100

    Start

---
+++ header.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 02:29:35 by abdnasse          #+#    #+#             */
+/*   Updated: 2025/06/20 02:29:48 by abdnasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H	
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+int	parse(int ac, char *av);
+
+#endif
